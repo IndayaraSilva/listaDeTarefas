eb5e121ce3cf161d08e504d91cfac2c8d7f7e9b5
IndayaraSilva <indayarasilva@gmail.com>
2022-05-20 23:12:13 -0300

    Otimizações no JS e na estilização - ainda não finalizado
    Co-authored-by: Kalini Martins  <KaliniMartins@users.noreply.github.com>
    Co-authored-by: Leandro Elias <oshirowiski0802@users.noreply.github.com>

--- control.js
+++ control.js
@@ -4,17 +4,22 @@ function mostrarMensagem() {
     itemDaLista.innerHTML = itemAdicionado.value; 
     document.getElementById("lista").append(itemDaLista);
     itemAdicionado.value = ""; 
-}
+
+    var novo = document.getElementById('modelo').content.cloneNode(true);
+    var li = novo.querySelector("li");
+
+    document.getElementById("lista").append(li).append(itemDaLista);
+
+    ocument.addEventListener("keypress"), function (e) {
+    if (e.key === 'Enter') {
+        mostrarMensagem()
+    }
+}};
+
 function adicionarBotao() {
     var campo = document.getElementByClass("editando")
     campo.value
     var novoBotao = document.createElement("Button");
     novoBotao.innerHTML = "Excluir"
 }
-document.addEventListener("keypress", function (e) {
-    if (e.key === 'Enter') {
-        mostrarMensagem()
-    }
-});
-
 
