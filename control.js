9cca131dedf09a034d6dc218ded390c1d724f158
IndayaraSilva <indayarasilva@gmail.com>
2022-05-23 23:31:44 -0300

    CSS todo atualizado e JavaScript quase pronto.
    Co-authored-by: Leandro Elias <oshirowiski0802@users.noreply.github.com>
    Co-authored-by: Kalini Martins  <KaliniMartins@users.noreply.github.com>

--- control.js
+++ control.js
@@ -1,25 +1,30 @@
-function mostrarMensagem() {
+var tarefasAdicionadas = [];
+
+document.addEventListener("keypress", function (e) {
+    if (e.key === 'Enter') {
+        inserirItemTemplate();
+    
+    }})
+
+function inserirItem(){
+     var itemAdicionado = document.getElementById("novoItem");
+     var itemDaLista = document.createElement("li")
+     itemDaLista.innerHTML = itemAdicionado.value; 
+     document.getElementById("lista").append(itemDaLista);
+     itemAdicionado.value = ""; 
+ }
+
+function inserirItemTemplate (){
     var itemAdicionado = document.getElementById("novoItem");
-    var itemDaLista = document.createElement("li")
-    itemDaLista.innerHTML = itemAdicionado.value; 
-    document.getElementById("lista").append(itemDaLista);
-    itemAdicionado.value = ""; 
+    tarefasAdicionadas.push(itemAdicionado.value);
 
     var novo = document.getElementById('modelo').content.cloneNode(true);
     var li = novo.querySelector("li");
+    li.append(itemAdicionado.value);
+    document.getElementById("lista").append(li);
+    itemAdicionado.value = "";
+ }
 
-    document.getElementById("lista").append(li).append(itemDaLista);
-
-    ocument.addEventListener("keypress"), function (e) {
-    if (e.key === 'Enter') {
-        mostrarMensagem()
-    }
-}};
-
-function adicionarBotao() {
-    var campo = document.getElementByClass("editando")
-    campo.value
-    var novoBotao = document.createElement("Button");
-    novoBotao.innerHTML = "Excluir"
-}
-
+ function removerItem(){
+    
+ }
